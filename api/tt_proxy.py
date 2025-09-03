0f9cb5c435ff929101a8171f749bfdb0bb95ad98
Emanuele Tomaello <emanuele.tomaello@dnb.no>
2025-09-03 19:59:25 +0200

    allowed cors

--- api/tt_proxy.py
+++ api/tt_proxy.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost", "https://hakey1408.github.io"])  # Restrict CORS to specific origins
 
 @app.route('/fetch-real-ttlink')
 def fetch_real_ttlink():
