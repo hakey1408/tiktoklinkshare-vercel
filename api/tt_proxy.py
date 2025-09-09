a9e0e20a144ab9120845e310e96b29909569e1c8
Emanuele Tomaello <emanuele.tomaello@dnb.no>
2025-09-09 10:28:13 +0200

    removed api key check

--- api/tt_proxy.py
+++ api/tt_proxy.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-#CORS(app, origins=["http://localhost:63342", "https://hakey1408.github.io"])  # Restrict CORS to specific origins
-CORS(app, origins=["*"])  # Restrict CORS to specific origins
+CORS(app, origins=["https://hakey1408.github.io"])  # Restrict CORS to specific origins
 
-API_KEY = os.environ.get("API_KEY")  # Set this in Vercel environment variables
 ALLOWED_REFERERS = ["https://hakey1408.github.io"]
 
 @app.before_request
@@ -18,11 +15,7 @@ def restrict_to_fetch_real_ttlink():
 
 @app.route('/fetch-real-ttlink')
 def fetch_real_ttlink():
-    # Authentication: Check API key
-    client_api_key = request.headers.get("X-API-KEY")
-    if not client_api_key or client_api_key != API_KEY:
-        return jsonify({'error': 'Unauthorized'}), 401
-    # Optional: Check Referer header
+    # Check Referer header
     referer = request.headers.get("Referer", "")
     if not any(referer.startswith(r) for r in ALLOWED_REFERERS):
         return jsonify({'error': 'Forbidden'}), 403
